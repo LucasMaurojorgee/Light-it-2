2b65bb577eb756b765ad23db904b8c032bf77bef
LucasMaurojorgee <lucas.maurojorge@gmail.com>
2023-06-14 16:30:15 -0300

    some new

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,32 +1,58 @@
 import React from "react";
 
 interface inputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  label: string;
-  placeholder: string;
-  type: string;
-  name: string;
-  error: any;
+  label?: string;
+  placeholder?: string;
+  type?: string;
+  name?: string;
+  error?: any;
+  options?: Array<string>;
 }
 
 const Input = React.forwardRef<HTMLInputElement, inputProps>(
-  ({ label, placeholder, type, error, ...props }, ref) => {
-    return (
-      <div className="p-3">
-        <label className="block text-sm font-medium leading-6 text-white">
-          {label}
-        </label>
-        <div className="mt-2">
-          <input
-            type={type}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-3"
-            placeholder={placeholder}
-            {...props}
-            ref={ref}
-          />
-          {error && <p>{error.message}</p>}
-        </div>
-      </div>
-    );
+  ({ label, placeholder, type, error, options, ...props }, ref) => {
+    switch (type) {
+      case "text":
+      case "email":
+        return (
+          <div className="p-3">
+            <label className="block text-sm font-medium leading-6 text-white">
+              {label}
+            </label>
+            <div className="mt-2">
+              <input
+                type={type}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-3"
+                placeholder={placeholder}
+                {...props}
+                ref={ref}
+              />
+              {error && <p>{error.message}</p>}
+            </div>
+          </div>
+        );
+        break;
+
+      case "select":
+        return (
+          <div>
+            <label
+              htmlFor="location"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              {label}
+            </label>
+            <select
+              className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder={placeholder}
+            >
+              {options?.map((e) => (
+                <option>{e}</option>
+              ))}
+            </select>
+          </div>
+        );
+    }
   }
 );
 
--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -8,11 +8,215 @@ import Input from "./Input";
 
 type formData = {
   name: string;
+  direccion: string;
+  email: string;
+  pais: string;
 };
 
 const UserSchema: z.ZodType<formData> = z.object({
-  name: z.string().min(2),
+  name: z.string().min(2).nonempty(),
+  direccion: z.string().min(2).nonempty(),
+  email: z.string().email().nonempty(),
+  pais: z.string().min(2).nonempty(),
 });
+
+const paises = [
+  "Afganistán",
+  "Albania",
+  "Alemania",
+  "Andorra",
+  "Angola",
+  "Antigua y Barbuda",
+  "Arabia Saudita",
+  "Argelia",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaiyán",
+  "Bahamas",
+  "Bangladés",
+  "Barbados",
+  "Baréin",
+  "Bélgica",
+  "Belice",
+  "Benín",
+  "Bielorrusia",
+  "Birmania",
+  "Bolivia",
+  "Bosnia y Herzegovina",
+  "Botsuana",
+  "Brasil",
+  "Brunéi",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Bután",
+  "Cabo Verde",
+  "Camboya",
+  "Camerún",
+  "Canadá",
+  "Catar",
+  "Chad",
+  "Chile",
+  "China",
+  "Chipre",
+  "Ciudad del Vaticano",
+  "Colombia",
+  "Comoras",
+  "Corea del Norte",
+  "Corea del Sur",
+  "Costa de Marfil",
+  "Costa Rica",
+  "Croacia",
+  "Cuba",
+  "Dinamarca",
+  "Dominica",
+  "Ecuador",
+  "Egipto",
+  "El Salvador",
+  "Emiratos Árabes Unidos",
+  "Eritrea",
+  "Eslovaquia",
+  "Eslovenia",
+  "España",
+  "Estados Unidos",
+  "Estonia",
+  "Etiopía",
+  "Filipinas",
+  "Finlandia",
+  "Fiyi",
+  "Francia",
+  "Gabón",
+  "Gambia",
+  "Georgia",
+  "Ghana",
+  "Granada",
+  "Grecia",
+  "Guatemala",
+  "Guyana",
+  "Guinea",
+  "Guinea ecuatorial",
+  "Guinea-Bisáu",
+  "Haití",
+  "Honduras",
+  "Hungría",
+  "India",
+  "Indonesia",
+  "Irak",
+  "Irán",
+  "Irlanda",
+  "Islandia",
+  "Islas Marshall",
+  "Islas Salomón",
+  "Israel",
+  "Italia",
+  "Jamaica",
+  "Japón",
+  "Jordania",
+  "Kazajistán",
+  "Kenia",
+  "Kirguistán",
+  "Kiribati",
+  "Kuwait",
+  "Laos",
+  "Lesoto",
+  "Letonia",
+  "Líbano",
+  "Liberia",
+  "Libia",
+  "Liechtenstein",
+  "Lituania",
+  "Luxemburgo",
+  "Madagascar",
+  "Malasia",
+  "Malaui",
+  "Maldivas",
+  "Malí",
+  "Malta",
+  "Marruecos",
+  "Mauricio",
+  "Mauritania",
+  "México",
+  "Micronesia",
+  "Moldavia",
+  "Mónaco",
+  "Mongolia",
+  "Montenegro",
+  "Mozambique",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Nicaragua",
+  "Níger",
+  "Nigeria",
+  "Noruega",
+  "Nueva Zelanda",
+  "Omán",
+  "Países Bajos",
+  "Pakistán",
+  "Palaos",
+  "Palestina",
+  "Panamá",
+  "Papúa Nueva Guinea",
+  "Paraguay",
+  "Perú",
+  "Polonia",
+  "Portugal",
+  "Reino Unido",
+  "República Centroafricana",
+  "República Checa",
+  "República de Macedonia",
+  "República del Congo",
+  "República Democrática del Congo",
+  "República Dominicana",
+  "República Sudafricana",
+  "Ruanda",
+  "Rumanía",
+  "Rusia",
+  "Samoa",
+  "San Cristóbal y Nieves",
+  "San Marino",
+  "San Vicente y las Granadinas",
+  "Santa Lucía",
+  "Santo Tomé y Príncipe",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leona",
+  "Singapur",
+  "Siria",
+  "Somalia",
+  "Sri Lanka",
+  "Suazilandia",
+  "Sudán",
+  "Sudán del Sur",
+  "Suecia",
+  "Suiza",
+  "Surinam",
+  "Tailandia",
+  "Tanzania",
+  "Tayikistán",
+  "Timor Oriental",
+  "Togo",
+  "Tonga",
+  "Trinidad y Tobago",
+  "Túnez",
+  "Turkmenistán",
+  "Turquía",
+  "Tuvalu",
+  "Ucrania",
+  "Uganda",
+  "Uruguay",
+  "Uzbekistán",
+  "Vanuatu",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Yibuti",
+  "Zambia",
+  "Zimbabue",
+];
 
 const UserForm = () => {
   const {
@@ -30,11 +234,36 @@ const UserForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Input
-        label="Name"
+        label="Full name"
         type="text"
-        placeholder="Lucas"
+        placeholder="Lucas Maurojorge"
         {...register("name")}
         error={errors.name}
+      />
+
+      <Input
+        label=" Email"
+        type="email"
+        placeholder="example@gmail.com"
+        {...register("email")}
+        error={errors.email}
+      />
+
+      <Input
+        label="Dirección"
+        type="text"
+        placeholder="Portugal 3417"
+        {...register("direccion")}
+        error={errors.direccion}
+      />
+
+      <Input
+        label="Pais"
+        type="select"
+        placeholder="Uruguay"
+        {...register("pais")}
+        error={errors.direccion}
+        options={paises}
       />
 
       <button type="submit">submit</button>
